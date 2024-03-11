3e0d918fa357483d6e5e4280524eb68687357283
baek min gyu <msmk908@naver.com>
2024-03-11 12:21:42 +0900

    2024-03-11 ver1

---
+++ SpringWork/ex00/src/main/java/com/codehows/sample/Chef.java
@@ -0,0 +1,11 @@
+package com.codehows.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Chef {
+	
+}
---
+++ SpringWork/ex01/src/main/java/org/codehows/domain/SampleDTO.java
@@ -0,0 +1,11 @@
+package org.codehows.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	
+	private String name;
+	private int age;
+
+}
